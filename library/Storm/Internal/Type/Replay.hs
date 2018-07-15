41715120c4e172728a12e8c1b7697b35ffb99175
Taylor Fausak <taylor@fausak.me>
2018-07-15 13:07:50 -0400

    Add a few more fields

--- library/Storm/Internal/Type/Replay.hs
+++ library/Storm/Internal/Type/Replay.hs
@@ -18,16 +18,23 @@ data Replay = Replay
   , replayUnknown4 :: U32.U32 -- 2
   , replayUnknown5 :: U32.U32 -- 4204439
   , replayUnknown6 :: Str.Str -- "Unsaved Replay" followed by a bunch of spaces
+  , replayUnknown7 :: U32.U32 -- 0
+  , replayUnknown8 :: U32.U32 -- apparently random
+  , replayUnknown9 :: U32.U32 -- maybe some type of timestamp?
   } deriving (Eq, Show)
 
 instance Json.FromJSON Replay where
   parseJSON =
-    Json.withObject "Replay" $ \o ->
-      Replay <$> Json.getKey o "unknown1" <*> Json.getKey o "unknown2" <*>
-      Json.getKey o "unknown3" <*>
-      Json.getKey o "unknown4" <*>
-      Json.getKey o "unknown5" <*>
-      Json.getKey o "unknown6"
+    Json.withObject "Replay" $ \o -> Replay
+      <$> Json.getKey o "unknown1"
+      <*> Json.getKey o "unknown2"
+      <*> Json.getKey o "unknown3"
+      <*> Json.getKey o "unknown4"
+      <*> Json.getKey o "unknown5"
+      <*> Json.getKey o "unknown6"
+      <*> Json.getKey o "unknown7"
+      <*> Json.getKey o "unknown8"
+      <*> Json.getKey o "unknown9"
 
 instance Json.ToJSON Replay where
   toJSON x =
@@ -38,6 +45,9 @@ instance Json.ToJSON Replay where
       , Json.setKey "unknown4" $ replayUnknown4 x
       , Json.setKey "unknown5" $ replayUnknown5 x
       , Json.setKey "unknown6" $ replayUnknown6 x
+      , Json.setKey "unknown7" $ replayUnknown7 x
+      , Json.setKey "unknown8" $ replayUnknown8 x
+      , Json.setKey "unknown9" $ replayUnknown9 x
       ]
 
 decodeReplay :: Decode.Decoder Replay
@@ -49,6 +59,9 @@ decodeReplay =
     <*> U32.decodeU32
     <*> U32.decodeU32
     <*> Str.decodeStr
+    <*> U32.decodeU32
+    <*> U32.decodeU32
+    <*> U32.decodeU32
 
 encodeReplay :: Encode.Encoder Replay
 encodeReplay x =
@@ -58,3 +71,6 @@ encodeReplay x =
     <> U32.encodeU32 (replayUnknown4 x)
     <> U32.encodeU32 (replayUnknown5 x)
     <> Str.encodeStr (replayUnknown6 x)
+    <> U32.encodeU32 (replayUnknown7 x)
+    <> U32.encodeU32 (replayUnknown8 x)
+    <> U32.encodeU32 (replayUnknown9 x)
